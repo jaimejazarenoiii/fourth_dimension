158f38e7435530beacf196986feda44d2d39f6e9
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2022-02-23 12:03:35 +0800

    Update movie page widget

--- lib/movies/movie/views/movie_page.dart
+++ lib/movies/movie/views/movie_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fourth_dimension/movies/movie/movie.dart';
 import 'package:fourth_dimension/movies/movies.dart';
 import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 
 class MoviePage extends StatelessWidget {
   const MoviePage({Key? key, required this.movie}) : super(key: key);
@@ -11,11 +14,38 @@ class MoviePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     return BlocProvider(
-        create: (_) => MovieBloc(httpClient: http.Client(), movie:
-            this.movie)..add(MovieFetched()),
-        child: Text('${movie.title}')
+      create: (_) => MovieBloc(httpClient: http.Client(), movie:
+          this.movie)..add(MovieFetched()),
+      child: Scaffold(
+          appBar: AppBar(
+              title: Text('${this.movie.title}')
+          ),
+          body: new Container(child: _mainWidget(context))
+      )
     );
   }
+
+  Widget _mainWidget(BuildContext context) {
+    final platform = Theme.of(context).platform;
+    switch (platform) {
+      case TargetPlatform.android:
+        return Text('Android phone');
+            // return widget on Android.
+      case TargetPlatform.iOS:
+        // return widget on iOS.
+        // This is used in the platform side to register the view.
+        const String viewType = 'SampleViewController';
+        // Pass parameters to the platform side.
+        final Map<String, dynamic> creationParams = <String, dynamic>{};
+        return UiKitView(
+                viewType: viewType,
+                layoutDirection: TextDirection.ltr,
+                creationParams: creationParams,
+                creationParamsCodec: const StandardMessageCodec(),
+            );
+      default: throw UnsupportedError('Unsupported platform view');
+    }
+  }
 }
+
